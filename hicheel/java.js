3a28255f0cc81f2de5bdf3fe41fe5459b80e8b05
Enkhmunkh1112 <lolrguy1@gmail.com>
2024-09-04 20:50:18 +0800

    update

---
+++ hicheel/java.js
@@ -0,0 +1,23 @@
+let name = "enkhmunkh";
+let age = 11;
+console.log(
+    "namaig" +name+ "gedeg.bi", +age+ "nastai", 
+)
+console.log(
+    "hello", "STRING"
+)
+console.log(
+    true,"Boolean"
+)
+console.log(
+    "name", "String"
+)
+console.log(
+"11", "Number"
+)
+  console.log("90","string"  )
+    console.log(90+"77","string"  )
+    console.log( {name:'Anu', age:15}, "Object" )
+    console.log(false,  "boolean" )
+    console.log(1655/5, "number"  )
+    console.log(["javascript", "html", "css"], "array") 
