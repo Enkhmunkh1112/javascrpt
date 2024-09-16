9dec904424aec66f8202ed2093498f04b105b76c
Enkhmunkh1112 <lolrguy1@gmail.com>
2024-09-16 20:28:12 +0800

    comiitrtion

--- hicheelloop/java.js
+++ hicheelloop/java.js
@@ -29,12 +29,78 @@
 
 // alert(nillberr)
 
-let u = prompt("numbers pls")
+// let u = prompt("numbers pls")
 
-let a =0;
+// let a =0;
 
-for (let u = 1; u < u; u++) {
+// for (let u = 1; u < u; u++) {
 
-    if(u%3==0 || u%5==0){a=a+u}
-}
-alert(u)
+//     if(u%3==0 || u%5==0){a=a+u}
+// }
+// alert(u)
+
+// for (let d = 1; d < 10; d++) {
+//     if(d % 3 === 0) {
+//         continue
+//     }else{console.log("loop" + d)}
+// }
+
+// h = prompt("too oruul");
+
+// let h = prompt("please numbers")
+
+// for (let j = 0; j < h; j++) {
+
+// }
+
+// let g = prompt("lol")
+// let l = "*"
+// let k = "*"
+
+// for (
+//  let h = 1; h < g; h++
+//  ) {
+
+//     console.log(l=l+k)
+
+// }
+
+// let z = prompt("plz me need numbas");
+
+// for (let x = 0; x < z; x++) {
+//   if (x % 2 !== 0 || x % 3 !== 0 || x % 5 !== 0 || x % 7 !== 0) {
+//     ;
+//   }
+// }
+
+// let fruits = "apple,orange,kiwi"
+
+// console.log(fruits)
+
+// console.log(
+//   fruits.split(',')
+// )
+
+// let fruits2 = prompt("jimsnii ner pls");
+
+// let array=fruits2.split(' ')
+
+// fruits2.push("banana");
+
+// console.log(fruits2);
+
+// let nothing = []
+
+// nothing.push('nothing1,nothing2,nothing3')
+
+// console.log(nothing)
+
+// let popper = prompt("pls words")
+
+// popper.prompt(",")
+
+// console.log(popper)
+
+let ani = prompt["name animals"];
+
+ani.splice(1, 2);
