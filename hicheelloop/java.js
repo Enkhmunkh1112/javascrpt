0bff6535cfc1ba10ad6fbad0198df051ade96607
Enkhmunkh1112 <lolrguy1@gmail.com>
2024-09-20 20:32:38 +0800

    JK

--- hicheelloop/java.js
+++ hicheelloop/java.js
@@ -101,6 +101,88 @@
 
 // console.log(popper)
 
-let ani = prompt["name animals"];
+// let ani = prompt["name animals"];
 
-ani.splice(1, 2);
+// ani.splice(1, 2);
+
+// let ww = prompt('numbers please');
+
+// ww = ww.split(',').map(num => num.trim());
+
+// for (let qq = 0; qq < ww.length; qq++) {
+
+//     console.log(ww[qq]);
+// }
+
+// let ee = prompt("3-t huvaagddag baigaag ogoi");
+
+// ee = ee.split(",").map((num) => num.trim());
+
+// for (let rr = 1; rr < ee.length; rr++) {
+//   if (rr % 3 === 0) {
+//     alert(rr);
+//     continue
+//   } else {
+
+//   }
+// }
+
+// let tt = prompt('too-g 3 ba 5 t huvaagddagu toonuudiig songoi')
+
+// for (let yy = 1; yy < tt.length; yy++) {
+//     if (yy % 3 ===0 || yy % 5 === 0){}
+// }
+let suragch = prompt("A, B, C, D ,F hemjeesuudiig oruulna uu.").toUpperCase();
+const students = [
+  { name: "Alice", point: 90 },
+  { name: "Bob", point: 55 },
+  { name: "Charlie", point: 78 },
+  { name: "David", point: 22 },
+  { name: "Eve", point: 68 },
+  { name: "Frank", point: 36 },
+  { name: "Grace", point: 95 },
+  { name: "Hannah", point: 89 },
+  { name: "Ivy", point: 83 },
+  { name: "Jack", point: 41 },
+  { name: "Katie", point: 87 },
+  { name: "Liam", point: 84 },
+  { name: "Mia", point: 79 },
+  { name: "Noah", point: 93 },
+  { name: "Olivia", point: 86 },
+  { name: "Peter", point: 57 },
+  { name: "Quinn", point: 94 },
+  { name: "Ryan", point: 80 },
+  { name: "Sophia", point: 75 },
+  { name: "Tyler", point: 46 },
+];
+let dun = [];
+for (let i = 0; i < students.length; i++) {
+  if (students[i].point > 100 && students[i].point <= 90 && suragch == "A") {
+    dun.push(students[i].name);
+  } else if (
+    students[i].point >= 80 &&
+    students[i].point < 90 &&
+    suragch == "B"
+  ) {
+    dun.push(students[i].name);
+  } else if (
+    students[i].point >= 70 &&
+    students[i].point < 80 &&
+    suragch == "C"
+  ) {
+    dun.push(students[i].name);
+  } else if (
+    students[i].point >= 60 &&
+    students[i].point < 70 &&
+    suragch == "D"
+  ) {
+    dun.push(students[i].name);
+  } else if (
+    students[i].point >= 0 &&
+    students[i].point < 60 &&
+    suragch == "F"
+  ) {
+    dun.push(students[i].name);
+  }
+}
+alert(dun);
