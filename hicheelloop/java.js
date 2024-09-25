bd94131b739b01c0da6c8665b5a4956f8db9d365
Enkhmunkh1112 <lolrguy1@gmail.com>
2024-09-25 19:52:57 +0800

    wtf i don fucking understand

--- hicheelloop/java.js
+++ hicheelloop/java.js
@@ -132,57 +132,168 @@
 // for (let yy = 1; yy < tt.length; yy++) {
 //     if (yy % 3 ===0 || yy % 5 === 0){}
 // }
-let suragch = prompt("A, B, C, D ,F hemjeesuudiig oruulna uu.").toUpperCase();
-const students = [
-  { name: "Alice", point: 90 },
-  { name: "Bob", point: 55 },
-  { name: "Charlie", point: 78 },
-  { name: "David", point: 22 },
-  { name: "Eve", point: 68 },
-  { name: "Frank", point: 36 },
-  { name: "Grace", point: 95 },
-  { name: "Hannah", point: 89 },
-  { name: "Ivy", point: 83 },
-  { name: "Jack", point: 41 },
-  { name: "Katie", point: 87 },
-  { name: "Liam", point: 84 },
-  { name: "Mia", point: 79 },
-  { name: "Noah", point: 93 },
-  { name: "Olivia", point: 86 },
-  { name: "Peter", point: 57 },
-  { name: "Quinn", point: 94 },
-  { name: "Ryan", point: 80 },
-  { name: "Sophia", point: 75 },
-  { name: "Tyler", point: 46 },
-];
-let dun = [];
-for (let i = 0; i < students.length; i++) {
-  if (students[i].point > 100 && students[i].point <= 90 && suragch == "A") {
-    dun.push(students[i].name);
-  } else if (
-    students[i].point >= 80 &&
-    students[i].point < 90 &&
-    suragch == "B"
-  ) {
-    dun.push(students[i].name);
-  } else if (
-    students[i].point >= 70 &&
-    students[i].point < 80 &&
-    suragch == "C"
-  ) {
-    dun.push(students[i].name);
-  } else if (
-    students[i].point >= 60 &&
-    students[i].point < 70 &&
-    suragch == "D"
-  ) {
-    dun.push(students[i].name);
-  } else if (
-    students[i].point >= 0 &&
-    students[i].point < 60 &&
-    suragch == "F"
-  ) {
-    dun.push(students[i].name);
+// let suragch = prompt("A, B, C, D ,F hemjeesuudiig oruulna uu.").toUpperCase();
+// const students = [
+//   { name: "Alice", point: 90 },
+//   { name: "Bob", point: 55 },
+//   { name: "Charlie", point: 78 },
+//   { name: "David", point: 22 },
+//   { name: "Eve", point: 68 },
+//   { name: "Frank", point: 36 },
+//   { name: "Grace", point: 95 },
+//   { name: "Hannah", point: 89 },
+//   { name: "Ivy", point: 83 },
+//   { name: "Jack", point: 41 },
+//   { name: "Katie", point: 87 },
+//   { name: "Liam", point: 84 },
+//   { name: "Mia", point: 79 },
+//   { name: "Noah", point: 93 },
+//   { name: "Olivia", point: 86 },
+//   { name: "Peter", point: 57 },
+//   { name: "Quinn", point: 94 },
+//   { name: "Ryan", point: 80 },
+//   { name: "Sophia", point: 75 },
+//   { name: "Tyler", point: 46 },
+// ];
+
+// let dun = [];
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].point > 100 && students[i].point <= 90 && suragch == "A") {
+//     dun.push(students[i].name);
+//   } else if (
+//     students[i].point >= 80 &&
+//     students[i].point < 90 &&
+//     suragch == "B"
+//   ) {
+//     dun.push(students[i].name);
+//   } else if (
+//     students[i].point >= 70 &&
+//     students[i].point < 80 &&
+//     suragch == "C"
+//   ) {
+//     dun.push(students[i].name);
+//   } else if (
+//     students[i].point >= 60 &&
+//     students[i].point < 70 &&
+//     suragch == "D"
+//   ) {
+//     dun.push(students[i].name);
+//   } else if (
+//     students[i].point >= 0 &&
+//     students[i].point < 60 &&
+//     suragch == "F"
+//   ) {
+//     dun.push(students[i].name);
+//   }
+// }
+// // alert(dun);
+
+// const students = [
+//   { name: "Alice", point: 90 },
+//   { name: "Bob", point: 55 },
+//   { name: "Charlie", point: 78 },
+//   { name: "David", point: 22 },
+//   { name: "Eve", point: 68 },
+//   { name: "Frank", point: 36 },
+//   { name: "Grace", point: 95 },
+//   { name: "Hannah", point: 89 },
+//   { name: "Ivy", point: 83 },
+//   { name: "Jack", point: 41 },
+//   { name: "Katie", point: 87 },
+//   { name: "Liam", point: 84 },
+//   { name: "Mia", point: 79 },
+//   { name: "Noah", point: 93 },
+//   { name: "Olivia", point: 86 },
+//   { name: "Peter", point: 57 },
+//   { name: "Quinn", point: 94 },
+//   { name: "Ryan", point: 80 },
+//   { name: "Sophia", point: 75 },
+//   { name: "Tyler", point: 46 },
+// ];
+
+// let shalgagch = prompt("unsan esvel unagu huuduudiig shalganuu");
+
+// let shalgagch2 = [];
+
+// let shalgagch3 = [];
+
+// for (let wasd = 0; wasd < students.length; wasd++) {
+//   if (students[wasd].point < 60) {
+//     shalgagch2.push(students[wasd].name);
+//   } else {
+//     shalgagch3.push(students[wasd].name);
+//   }
+// }
+
+// if ((shalgagch === "unsan")) {
+//   alert(shalgagch2);
+// } else if ((shalgagch === "unagu")) {
+//   alert(shalgagch3);
+// } else {
+//   alert("what.");
+// }
+
+// let B = 10;
+// let C = 20;
+// let D = 5;
+// console.log("A=", B * C - D);
+
+// const A = (B, C, D) => {
+//   return B * C - D;
+// };
+// console.log(A(20, 10, 56));
+
+// const ttime = (E, F) => {
+//   return E+(F*24)
+// }
+
+// console.log(ttime(10,3))
+
+// let H = 127
+// let G=83
+// if (G > H) {
+//   console.log(G);
+// } else if (G < H) {
+//   console.log(H);
+// } else {
+//   console.log("tentsuu bna");
+// }
+
+// const idk=(J, K)=>{
+//   return (J+K)*2
+// }
+
+// console.log (idk(10,10))
+
+// const checkPoint = (S) => {
+//   if (S >= 90) {
+//     return "A";
+//   } else if (S >= 80 && S <= 89) {
+//     return "B";
+//   } else if (S >= 61 && S <= 79) {
+//     return "C";
+//   } else if (S >= 0 && S <= 60) {
+//     return "F";
+//   }
+// };
+// console.log("Grade=", checkPoint(153));
+
+const FLE = (A, B, C, D, E, F) => {
+  if (A > B && A > C && A > D && A > E && A > F ) {
+    return (A);
+  } else if (B > C && B > D && B > E && B > F &&  B > A) {
+    return (B);
+  }else if (C > B && C > A && C > D && C > E && C > F) {
+    return (C);
+  } else if (D > C && D > B && D > E && D > F && D > A) {
+    return (D);
+  }else if (E > B && E > C && E > D && E > A && E > F) {
+    return (E);
+  } else if (B > C && B > D && B > E && B > F && B > A) {
+    return (F);
   }
-}
-alert(dun);
+};
+
+let Han = [1, 4, 7, 3, 5, 8]
+
+console.log("the largest element (here)is:", FLE(Han) )
